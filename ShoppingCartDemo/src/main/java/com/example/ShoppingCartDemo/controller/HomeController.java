ebf3393273ae86810da9f3f7fafe8b535dd90454
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-08-21 01:33:41 +0530

    update operation in customer

--- ShoppingCartDemo/src/main/java/com/example/ShoppingCartDemo/controller/HomeController.java
+++ ShoppingCartDemo/src/main/java/com/example/ShoppingCartDemo/controller/HomeController.java
@@ -180,5 +180,35 @@ public class HomeController {
 		}
 		return "redirect:items";
 	}
+	
+	//update customer
+	
+	@GetMapping("/updcust")
+	public String upd_cust(@RequestParam("customer")String name,Model model) {
+		if(name!=null)
+		{
+			int pos=shoppingCart.check_cust(name);
+			if(pos>=0) {
+				Customer customer=shoppingCart.getCustomers().get(pos);
+				model.addAttribute("customer", customer);
+				return "cust_update";
+			}
+		}
+		return "init";
+	}
 
+	@PostMapping("/updatecust")
+	public String savecust(@RequestParam("name")String[] name,@ModelAttribute("customer") Customer customer) {
+		if(customer.getName()!=null && customer.getContact()!=null) {
+			
+			int pos=shoppingCart.check_cust(name[0]);
+			if(pos>=0) {
+				Customer customer1=shoppingCart.getCustomers().get(pos);
+				customer1.setName(name[1]);
+				customer1.setContact(customer.getContact());
+				return "redirect:init";
+			}
+		}
+		return "redirect:init";
+	}
 }
